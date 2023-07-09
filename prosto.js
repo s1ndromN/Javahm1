9839828d63dc58b831c70925b907cc126498a9b0
Ruslan Lytvynchuk <weblitvinchuk@gmail.com>
2023-07-09 22:13:32 +0300

    Javahm1

---
+++ prosto.js
@@ -0,0 +1,57 @@
+// homework_1
+
+// №1
+let number = "12";
+
+console.log(number);
+
+// №2
+// let name = "Nikita";
+
+// console.log(name);
+
+// №3
+let isStudent = true;
+
+console.log(isStudent);
+
+// №4
+let massage =
+	"«Чим розумніша людина, тим легше вона визнає себе дурнем». Альберт Ейнштейн";
+
+console.log(massage);
+
+// №5
+let myNumber = 10;
+
+myNumber = myNumber + 10;
+
+console.log(myNumber);
+
+// №6
+let myNull = null;
+
+console.log(myNull);
+
+// №7
+const name = prompt("Ваше ім'я");
+
+console.log("Hello", name);
+
+// №8
+let isConfirm = confirm();
+
+if (isConfirm) {
+	console.log("Дякую за підтвердження");
+} else {
+	console.log("Дію відмінено!");
+}
+
+// №9
+let isWarning = confirm("Дія небезпечна");
+
+if (isWarning) {
+	console.log("Дякую за підтвердження");
+} else {
+	console.log("Дію відмінено");
+}
