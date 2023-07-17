932be358761c2af777e1a8ba05597bb48bb7f55a
Ruslan Lytvynchuk <weblitvinchuk@gmail.com>
2023-07-17 12:59:19 +0300

    javahw2

--- prosto.js
+++ prosto.js
@@ -1,57 +1,132 @@
 // homework_1
 
 // №1
-let number = "12";
+// let number = "12";
 
-console.log(number);
+// console.log(number);
+
+// // №2
+// // let name = "Nikita";
+
+// // console.log(name);
+
+// // №3
+// let isStudent = true;
+
+// console.log(isStudent);
+
+// // №4
+// let massage =
+// 	"«Чим розумніша людина, тим легше вона визнає себе дурнем». Альберт Ейнштейн";
+
+// console.log(massage);
+
+// // №5
+// let myNumber = 10;
+
+// myNumber = myNumber + 10;
+
+// console.log(myNumber);
+
+// // №6
+// let myNull = null;
+
+// console.log(myNull);
+
+// // №7
+// const name = prompt("Ваше ім'я");
+
+// console.log("Hello", name);
+
+// // №8
+// let isConfirm = confirm();
+
+// if (isConfirm) {
+// 	console.log("Дякую за підтвердження");
+// } else {
+// 	console.log("Дію відмінено!");
+// }
+
+// // №9
+// let isWarning = confirm("Дія небезпечна");
+
+// if (isWarning) {
+// 	console.log("Дякую за підтвердження");
+// } else {
+// 	console.log("Дію відмінено");
+// }
+
+// homework_2
+
+// №1
+
+let Celsius = 29;
+
+let Fahrenheit = (Celsius + 40) * 1.8 - 40;
+
+console.log("Celsius - ", Celsius, "Fahrenheit -", Fahrenheit);
 
 // №2
-// let name = "Nikita";
 
-// console.log(name);
+let days = 31;
+
+let hours = days * 24;
+
+let minuts = hours * 60;
+
+console.log("Hours - ", hours, "Minuts -", minuts);
 
 // №3
-let isStudent = true;
 
-console.log(isStudent);
+let helth = 100;
+
+let energy = 100;
+
+helth = helth - 20;
+
+energy = energy - 20;
+
+console.log("Helth - ", helth, "Energy -", energy);
 
 // №4
-let massage =
-	"«Чим розумніша людина, тим легше вона визнає себе дурнем». Альберт Ейнштейн";
 
-console.log(massage);
+let sum = 100;
+
+let discount = 10;
+
+let total = sum - (sum * discount) / 100;
+
+console.log("Sum - ", sum, "Discount -", discount, "Total -", total);
 
 // №5
-let myNumber = 10;
 
-myNumber = myNumber + 10;
+let number = 15.65;
 
-console.log(myNumber);
+console.log(Math.floor(number));
 
 // №6
-let myNull = null;
 
-console.log(myNull);
+let number2 = 15.6;
+
+console.log(parseFloat(number2));
 
 // №7
-const name = prompt("Ваше ім'я");
 
-console.log("Hello", name);
+let number3 = "15.667";
+
+console.log(parseInt(number3));
 
 // №8
-let isConfirm = confirm();
 
-if (isConfirm) {
-	console.log("Дякую за підтвердження");
-} else {
-	console.log("Дію відмінено!");
-}
+let number4 = "9";
+
+console.log(Math.sqrt(number4));
 
 // №9
-let isWarning = confirm("Дія небезпечна");
 
-if (isWarning) {
-	console.log("Дякую за підтвердження");
-} else {
-	console.log("Дію відмінено");
-}
+let number5 = 56.789;
+
+let string6 = "45.346";
+
+console.log("number5 - ", number5.toString());
+console.log("string6 - ", parseInt(string6));
